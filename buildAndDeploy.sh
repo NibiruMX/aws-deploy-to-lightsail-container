44bcea60e84c567cb13ce458fb9a5494566e21d1
AYM1607 <u.g.a.mariano@gmail.com>
2021-02-26 00:57:51 -0500

    Added hints.

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -2,8 +2,16 @@ echo "Building and publishing"
 
 docker build -t convee/api $GITHUB_WORKSPACE
 
+echo "Done building"
+
 aws lightsail push-container-image --service-name conveeapi --label pipeline --image convee/api
+
+echo "Done pushing container"
 
 PIPELINE_IMAGE_TAG=$(aws lightsail get-container-images --service conveeapi | jq -r .containerImages[0].image)
 
+echo "Using image $PIPELINE_IMAGE_TAG"
+
 aws lightsail create-container-service-deployment --service-name conveeapi --containers "{\"conveeapi\":{\"image\":\"$PIPELINE_IMAGE_TAG\",\"ports\":{\"80\":\"HTTP\"},\"environment\":{\"DATABASE_URL\":\"$1\"}}}" --public-endpoint "{\"containerName\":\"conveeapi\",\"containerPort\":80}" > /dev/null
+
+echo "Done creating deployment"
